22e38c94423223cdf31192d942e259d991797f57
Ubuntu <yvvanvo@gmail.com>
2021-12-30 03:22:48 -0800

    Added cmake files

---
+++ CMakeFiles/vo-vy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vo-vy.dir/vo-vy.cpp.o"
+  "vo-vy.pdb"
+  "vo-vy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vo-vy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
